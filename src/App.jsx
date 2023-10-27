3abfe2c100c6604d4516f578550c16e22ef18fbb
Magnus-Alexander <kodhodetmagnus@gmail.com>
2023-10-27 11:34:06 +0200

    actually works so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState("");
   const [modifier, setModifier] = useState("");
+  const [modifier2, setModifier2] = useState("");
   const [count2, setCount2] = useState("");
-  // const [setResult] = useState();
+  let [result, setResult] = useState("");
 
-  let [addCount, setAddCount] = useState(0);
+  const [string, setString] = useState("");
+  useEffect(() => {
+    setString(count + modifier + count2 + modifier2 + result);
+  }, [count, modifier, count2, modifier2, result]);
+  console.log(`string is: ${string}`);
+
+  const [addCount, setAddCount] = useState(0);
   function add() {
     count != "" ? setAddCount(addCount + 1) : console.log("");
     addCount < 1 &&
     count != "" &&
     modifier != "-" &&
-    modifier != "X" &&
+    modifier != "x" &&
     modifier != "/"
       ? setModifier(modifier + "+")
       : console.log("Too many pluses");
   }
+
   let [subCount, setSubCount] = useState(0);
   function subtract() {
     count != "" ? setSubCount(subCount + 1) : console.log("");
     subCount < 1 &&
     count != "" &&
     modifier != "+" &&
-    modifier != "X" &&
+    modifier != "x" &&
     modifier != "/"
       ? setModifier(modifier + "-")
       : console.log("Too many minuses");
   }
+
   let [multiCount, setMultiCount] = useState(0);
   function multiply() {
     count != "" ? setMultiCount(multiCount + 1) : console.log("");
@@ -37,9 +46,10 @@ function App() {
     modifier != "+" &&
     modifier != "-" &&
     modifier != "/"
-      ? setModifier(modifier + "X")
+      ? setModifier(modifier + "x")
       : console.log("Too many multiplication symbols");
   }
+
   let [divCount, setDivCount] = useState(0);
   function divide() {
     count != "" ? setDivCount(divCount + 1) : console.log("");
@@ -47,60 +57,136 @@ function App() {
     count != "" &&
     modifier != "+" &&
     modifier != "-" &&
-    modifier != "X"
+    modifier != "x"
       ? setModifier(modifier + "/")
       : console.log("Too many division symbols");
   }
+
   function reset() {
     setCount("");
     setCount2("");
     setModifier("");
-    setAddCount((addCount = 0));
-    setSubCount((subCount = 0));
-    setMultiCount((multiCount = 0));
-    setDivCount((divCount = 0));
+    setAddCount(0);
+    setSubCount(0);
+    setMultiCount(0);
+    setDivCount(0);
+    setCommaCount(0);
+    setCommaCount2(0);
+    setResult("");
+    setModifier2("");
   }
-  function del() {}
 
-  function one() {
-    setCount(count + "1");
-  }
-  function two() {
-    setCount(count + "2");
-  }
-  function three() {
-    setCount(count + "3");
-  }
-  function four() {
-    setCount(count + "4");
-  }
-  function five() {
-    setCount(count + "5");
-  }
-  function six() {
-    setCount(count + "6");
-  }
-  function seven() {
-    setCount(count + "7");
-  }
-  function eight() {
-    setCount(count + "8");
-  }
-  function nine() {
-    setCount(count + "9");
+  function del() {
+    let newString = string.slice(0, -1);
+    setString(newString);
+    let parts = newString.split(/([+\-x/])/);
+    setCount(parts[0]);
+    setModifier(parts[1] || "");
+    setCount2(parts[2] || "");
   }
   function zero() {
-    setCount(count + "0");
+    modifier == ""
+      ? setCount(count + "0")
+      : modifier != ""
+      ? setCount2(count2 + "0")
+      : console.log(string);
   }
-  const [commaCount, setCommaCount] = useState(0);
+
+  function one() {
+    modifier == ""
+      ? setCount(count + "1")
+      : modifier != ""
+      ? setCount2(count2 + "1")
+      : console.log(string);
+  }
+
+  function two() {
+    modifier == ""
+      ? setCount(count + "2")
+      : modifier != ""
+      ? setCount2(count2 + "2")
+      : console.log(string);
+  }
+
+  function three() {
+    modifier == ""
+      ? setCount(count + "3")
+      : modifier != ""
+      ? setCount2(count2 + "3")
+      : console.log(string);
+  }
+
+  function four() {
+    modifier == ""
+      ? setCount(count + "4")
+      : modifier != ""
+      ? setCount2(count2 + "4")
+      : console.log(string);
+  }
+
+  function five() {
+    modifier == ""
+      ? setCount(count + "5")
+      : modifier != ""
+      ? setCount2(count2 + "5")
+      : console.log(string);
+  }
+
+  function six() {
+    modifier == ""
+      ? setCount(count + "6")
+      : modifier != ""
+      ? setCount2(count2 + "6")
+      : console.log(string);
+  }
+
+  function seven() {
+    modifier == ""
+      ? setCount(count + "7")
+      : modifier != ""
+      ? setCount2(count2 + "7")
+      : console.log(string);
+  }
+
+  function eight() {
+    modifier == ""
+      ? setCount(count + "8")
+      : modifier != ""
+      ? setCount2(count2 + "8")
+      : console.log(string);
+  }
+
+  function nine() {
+    modifier == ""
+      ? setCount(count + "9")
+      : modifier != ""
+      ? setCount2(count2 + "9")
+      : console.log(string);
+  }
+
+  let [commaCount, setCommaCount] = useState(0);
+  let [commaCount2, setCommaCount2] = useState(0);
   function comma() {
-    setCommaCount(commaCount + 1);
     console.log(commaCount);
-    if (commaCount < 1) {
-      setCount(count + ",");
-    } else {
-      console.log("too many commas");
-    }
+    count != "" ? setCommaCount(commaCount + 1) : console.log("");
+    commaCount < 1 && count != "" && modifier == ""
+      ? setCount(count + ",")
+      : modifier != "" && count2 !== "" && commaCount2 < 1
+      ? (setCount2(count2 + ","), setCommaCount2(commaCount2 + 1))
+      : console.log("too many commas");
+  }
+
+  function equals() {
+    modifier == "+"
+      ? (setModifier2(modifier2 + "="),
+        setResult((result = parseInt(count) + parseInt(count2))))
+      : modifier == "-"
+      ? setResult(parseInt(count) - parseInt(count2))
+      : modifier == "x"
+      ? setResult(parseInt(count) * parseInt(count2))
+      : modifier == "/"
+      ? setResult(parseInt(count) / parseInt(count2))
+      : console.log(string);
   }
   return (
     <>
@@ -108,9 +194,7 @@ function App() {
       <div className="space"></div>
       <div className="calcContainer">
         <div className="resultScreen">
-          <p>
-            {count} {modifier} {count2}
-          </p>
+          <p>{string}</p>
         </div>
         <div className="space"></div>
         <div className="buttons">
@@ -131,12 +215,14 @@ function App() {
           <button onClick={comma}>,</button>
           <button onClick={zero}>0</button>
           <button onClick={divide}>/</button>
-          <button onClick={multiply}>X</button>
+          <button onClick={multiply}>x</button>
           <div className="resetEquals">
             <button className="reset" onClick={reset}>
               Reset
             </button>
-            <button className="equals">=</button>
+            <button onClick={equals} className="equals">
+              =
+            </button>
           </div>
         </div>
       </div>
