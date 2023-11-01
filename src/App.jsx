733f5609fcf2916f792d032fd0c4d34553801fac
Magnus-Alexander <kodhodetmagnus@gmail.com>
2023-11-01 12:47:54 +0100

    Finished calculator

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       : console.log("Too many pluses");
   }
 
-  let [subCount, setSubCount] = useState(0);
+  const [subCount, setSubCount] = useState(0);
   function subtract() {
     count != "" ? setSubCount(subCount + 1) : console.log("");
     subCount < 1 &&
@@ -35,7 +35,7 @@ function App() {
       : console.log("Too many minuses");
   }
 
-  let [multiCount, setMultiCount] = useState(0);
+  const [multiCount, setMultiCount] = useState(0);
   function multiply() {
     count != "" ? setMultiCount(multiCount + 1) : console.log("");
     multiCount < 1 &&
@@ -47,7 +47,7 @@ function App() {
       : console.log("Too many multiplication symbols");
   }
 
-  let [divCount, setDivCount] = useState(0);
+  const [divCount, setDivCount] = useState(0);
   function divide() {
     count != "" ? setDivCount(divCount + 1) : console.log("");
     divCount < 1 &&
@@ -69,13 +69,14 @@ function App() {
     setDivCount(0);
     setCommaCount(0);
     setCommaCount2(0);
-    setResult("");
+    setResult(null);
   }
 
   function del() {
     let newString = string.slice(0, -1);
     setString(newString);
     let parts = newString.split(/([+\-x/])/);
+    console.log(parts);
     setCount(parts[0]);
     setModifier(
       parts[1] || ("" && setAddCount(0)),
@@ -84,92 +85,92 @@ function App() {
       setDivCount(0)
     );
     setCount2(parts[2] || "");
-    setResult(parts[4] || "");
+    setResult(parts[4] || null);
 
-    modifier == "" || result != ""
+    modifier == "" || result != null
       ? (setAddCount(0), setSubCount(0), setMultiCount(0), setDivCount(0))
       : console.log("");
   }
   function zero() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "0")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "0")
       : console.log(string);
     console.log(string.length + 1);
   }
   function one() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "1")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "1")
       : console.log(string);
   }
   function two() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "2")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "2")
       : console.log(string);
   }
   function three() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "3")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "3")
       : console.log(`${"the string is "} ${string}`);
   }
   function four() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "4")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "4")
       : console.log(string);
   }
   function five() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "5")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "5")
       : console.log(string);
   }
   function six() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "6")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "6")
       : console.log(string);
   }
   function seven() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "7")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "7")
       : console.log(string);
   }
   function eight() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "8")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "8")
       : console.log(string);
   }
   function nine() {
-    modifier == "" && string.length < 11 && result == ""
+    modifier == "" && string.length < 11 && result == null
       ? setCount(count + "9")
-      : modifier != "" && string.length < 11 && result == ""
+      : modifier != "" && string.length < 11 && result == null
       ? setCount2(count2 + "9")
       : console.log(string);
   }
 
-  let [commaCount, setCommaCount] = useState(0);
-  let [commaCount2, setCommaCount2] = useState(0);
+  const [commaCount, setCommaCount] = useState(0);
+  const [commaCount2, setCommaCount2] = useState(0);
   function comma() {
     console.log(commaCount);
     count != "" ? setCommaCount(commaCount + 1) : console.log("");
-    commaCount < 1 && count != "" && modifier == "" && result == ""
+    commaCount < 1 && count != "" && modifier == "" && result == null
       ? setCount(count + ".")
-      : modifier != "" && count2 !== "" && commaCount2 < 1 && result == ""
+      : modifier != "" && count2 !== "" && commaCount2 < 1 && result == null
       ? (setCount2(count2 + "."), setCommaCount2(commaCount2 + 1))
       : console.log("too much punctuation");
   }
@@ -201,24 +202,35 @@ function App() {
   return (
     <>
       <section className="header">
-        <button onClick={themeOne} className="button1">
-          <p>1</p>
-        </button>
-        <button onClick={themeTwo} className="button2">
-          <p>2</p>
-        </button>
-        <button onClick={themeThree} className="button3">
-          <p>3</p>
-        </button>
+        <div className="themeButtons">
+          <button onClick={themeOne} className="button1">
+            <p>1</p>
+          </button>
+          <button onClick={themeTwo} className="button2">
+            <p>2</p>
+          </button>
+          <button onClick={themeThree} className="button3">
+            <p>3</p>
+          </button>
+          <div className="space2"></div>
+        </div>
+        <div className="calcName">
+          <p>calc</p>
+        </div>
         <div className="THEME">
           <p>THEME</p>
           <div className="space"></div>
           <div className="slide">
+            <div className="themeButtons2">
+              <button onClick={themeOne}></button>
+              <button onClick={themeTwo}></button>
+              <button onClick={themeThree}></button>
+            </div>
             <div className="dot"></div>
           </div>
         </div>
       </section>
-      <p>calc</p>
+
       <div className="space3"></div>
       <div className="calcContainer">
         <div className="resultScreen">
@@ -247,18 +259,19 @@ function App() {
           <button onClick={zero}>0</button>
           <button onClick={divide}>/</button>
           <button onClick={multiply}>x</button>
-          <div className="resetEquals">
-            <button className="reset" onClick={reset}>
-              Reset
-            </button>
-            <button onClick={equals} className="equals">
-              =
-            </button>
-          </div>
+        </div>
+        <div className="resetEquals">
+          <button className="reset" onClick={reset}>
+            Reset
+          </button>
+          <button onClick={equals} className="equals">
+            =
+          </button>
         </div>
       </div>
       <style>{`
         @import url("${themeState}.css");
+      
       `}</style>
     </>
   );
